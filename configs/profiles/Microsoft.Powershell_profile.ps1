26ba81b14fbc7691b0354dbe3c2940652c9120ce
Tired <125549280+TiredGui@users.noreply.github.com>
2023-06-02 16:21:54 +0200

    Reworked the fkiign powershell profilre

--- configs/profiles/Microsoft.Powershell_profile.ps1
+++ configs/profiles/Microsoft.Powershell_profile.ps1
@@ -1,39 +1,56 @@
-# Functions
+# Transitientprompt
 Function Invoke-Starship-TransientFunction {
     &starship module character
 }
+# Functions
 
-function degit {
-    Remove-Item -Recurse -Force .git
-    echo "Removed Git Repository"
-}
-
-function dieHist {
+# Deletes command history jic
+Function dieHist {
     Remove-Item -Recurse -Force C:\Users\matti\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
     echo "History removed"
 }
 
-function instacommit {
-    git add .
-    git commit
-    git pull
+# Git shit
+
+# Remove git repo
+Function degit {
+    $dir = $args[0]
+    if ($null -eq $dir) {
+        echo "No directory supplied"
+        break
+    }
+    Remove-Item -Recurse -Force "$dir/.git"
+    if ("." -eq $dir) {
+        echo "Removed Git Repository at the current directory"    
+    } else {
+        echo "Removed Git Repository at $dir"
+    }
 }
 
+Function gitsync {
+    git push --quiet
+    git pull --quiet
+    echo "Done"
+}
+
+# Touch thing
 Function touch {
     $file = $args[0]
     if ($null -eq $file) {
-        throw "No filename supplied"
+        echo "No filename supplied"
+        break
     }
     New-Item -ItemType file $file
 }
 
 # Aliases
-Set-Alias grep findstr # findstr is grep
-Set-Alias bash sh # MINGW shell alias
+Set-Alias grep findstr         # findstr is grep
+Set-Alias bash sh              # MINGW shell alias
+Set-Alias make mingw32-make    # MINGW make alias
+Set-Alias zip Compress-Archive # So i can just fukin type "zip" instead of fukmfna "Compress-Archive"
 
 # Expressions
-Invoke-Expression (&scoop-search --hook)
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (&starship init powershell) # Prompt
 
 # Transitientprompt
 Enable-TransientPrompt
